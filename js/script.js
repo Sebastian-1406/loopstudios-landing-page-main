8e95557de8720d33eaba9b2248f6036a6bb48706
Sebastian <SCsebastian1406@gmail.com>
2021-10-08 20:23:33 -0500

    nav-hambuger agregado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-let iconHamburger = document.querySelector(".nav__icon-hamburger");
+let iconHamburger = document.querySelector(".icon-hamburger");
 
 iconHamburger.addEventListener("click", () => {
     let nav = document.querySelector(".nav");
     nav.classList.toggle("nav-activo")
     if(nav.classList[1] == "nav-activo"){
-        document.querySelector("body").style.overflow = "hidden"
+        document.querySelector("body").style.overflow = "hidden";
+        iconHamburger.src = "./images/icon-close.svg";
     }else{
-        document.querySelector("body").style.overflow = "auto"
+        document.querySelector("body").style.overflow = "auto";
+        iconHamburger.src = "./images/icon-hamburger.svg";
     }
 })
